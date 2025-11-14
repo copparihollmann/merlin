f860b95baad64924beb63a45728eebc4a30ef764
Agustin N. Coppari Hollmann <copparihollmann@gmail.com>
2025-11-14 11:51:29 -0800

    Updated runtime plugin for samples

--- iree_runtime_plugin.cmake
+++ iree_runtime_plugin.cmake
@@ -3,3 +3,5 @@ list(APPEND CMAKE_MODULE_PATH "${CMAKE_CURRENT_LIST_DIR}/cmake")
 set(IREE_ITA_SOURCE_DIR "${CMAKE_CURRENT_LIST_DIR}")
 
 add_subdirectory(${CMAKE_CURRENT_LIST_DIR}/samples merlin-samples)
+
+#add_subdirectory(${CMAKE_CURRENT_LIST_DIR}/ukernels merlin-ukernels)
